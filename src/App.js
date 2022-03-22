fdce7ac3a6d341a2fb74672d12ff58c5086bee0f
codu2 <khub0929@gmail.com>
2022-03-23 02:19:06 +0900

    Navbar, Card 컴포넌트 추가 및 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 
+const DUMMY_POST = [
+  {
+    id: 1,
+    username: "john",
+    fullname: "John keller",
+    userImg:
+      "https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    postImg:
+      "https://images.unsplash.com/photo-1547499681-28dece7dba00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+  {
+    id: 2,
+    username: "monica",
+    fullname: "Monica stan",
+    userImg:
+      "https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    postImg:
+      "https://images.unsplash.com/photo-1606484589718-070f61cbd72d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80",
+  },
+];
+
 function App() {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
@@ -14,7 +35,10 @@ function App() {
       {user ? (
         <>
           <Navbar />
-          <Card />
+          {DUMMY_POST.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
+          <span className="username">{username}</span>
         </>
       ) : (
         <div className="login">
